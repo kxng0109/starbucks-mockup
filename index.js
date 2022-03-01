1c555c8d1cbd31ebda077f11e1392c02bbe84ef5
kxng0109 <iluhjosh13@gmail.com>
2022-03-01 18:40:21 +0100

    Fixed navigation for mobile.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const container = document.querySelector('#container');
 const closedMenu = document.querySelector('.menu-closed');
 const nav = document.querySelector('#nav');
 const nav2 = document.querySelector('#nav2');
@@ -10,16 +9,18 @@ closedMenu.onclick = () =>{
 	if (closedMenu.getAttribute('name') === 'menu-outline') {
 		closedMenu.setAttribute('name', 'close-outline');
 		nav.style.animation = 'flyIn 0.15s ease-in forwards';
-		container.style.pointerEvents = 'none';
-		// container.style.filter = 'brightness(60%)';
+		document.body.style.overflowY = 'hidden';
+		main.style.filter = 'brightness(60%)';
+		window.scrollTo(0, 0);
 	} else {
 		closedMenu.setAttribute('name', 'menu-outline');
 		nav2.style.animation = 'flyOut 0.15s ease-out forwards';
 		nav.style.animation = 'flyOut 0.15s ease-out forwards';
-		container.style.pointerEvents = 'auto';
-		// container.style.filter = 'brightness(100%)';
+		document.body.style.overflowY = 'auto';
+		main.style.filter = 'brightness(100%)';
 	}
 }
 
 clickableNav.onclick = () =>{
+	nav2.style.animation = 'flyIn 0.15s ease-in forwards';
 }
