12a6fc0078f165e55f774c25112ebd1e1573c3ea
kxng0109 <iluhjosh13@gmail.com>
2022-03-03 22:22:07 +0100

    Started working on other views apart from mobile. Haven't fixed the issue on mobile yet.

--- index.js
+++ index.js
@@ -6,7 +6,18 @@ const nav2Header = document.querySelector('#nav2-header');
 const main = document.querySelector('#main');
 const hiddenDiv = document.querySelectorAll('.hidden-div');
 const dropdownDiv = document.querySelectorAll('.dropdown-divs');
+const toggleIcon = document.querySelectorAll('.toggle');
+const outerHiddenDiv = document.querySelectorAll('.youknow');
+const hiddenLinks = document.querySelectorAll('.hidden-links');
 
+let hdHeight = [];
+hiddenDiv.forEach( (element, index) => {
+	hdHeight[index] = hiddenDiv[index].offsetHeight;
+	// hiddenDiv[index].style.zIndex = '-1';
+	hiddenDiv[index].style.position = 'absolute';
+	hiddenDiv[index].style.top = `-${hdHeight[index]}px`;
+	outerHiddenDiv[index].style.height = '0px';
+});
 
 closedMenu.onclick = () =>{
 	if (closedMenu.getAttribute('name') === 'menu-outline') {
@@ -17,7 +28,13 @@ closedMenu.onclick = () =>{
 		window.scrollTo(0, 0);
 	} else {
 		closedMenu.setAttribute('name', 'menu-outline');
-		nav2.style.animation = 'flyOut 0.15s ease-out forwards';
+		switch (nav2.style.animation === '') {
+			case false:
+				nav2.style.animation = 'flyOut 0.15s ease-out forwards';
+				break;
+			default:
+				break;
+		}		
 		nav.style.animation = 'flyOut 0.15s ease-out forwards';
 		document.body.style.overflowY = 'auto';
 		main.style.filter = 'brightness(100%)';
@@ -34,15 +51,25 @@ nav2Header.onclick = () =>{
 
 dropdownDiv.forEach( (element, index) =>{
 	dropdownDiv[index].onclick = () =>{
-		console.log(hiddenDiv[index].offsetHeight)
-		if (hiddenDiv[index].style.height === '' || hiddenDiv[index].style.height === '0px') {
-			hiddenDiv[index].style.visibility = 'visible';
-			hiddenDiv[index].style.zIndex = '0';
-			hiddenDiv[index].style.height = 'auto';
-		}else {			
-			hiddenDiv[index].style.zIndex = '-1';
-			hiddenDiv[index].style.height = '0';
-			hiddenDiv[index].style.visibility = 'hidden';
+		if (outerHiddenDiv[index].style.height === '' || outerHiddenDiv[index].style.height === '0px') {
+			outerHiddenDiv[index].style.height = `${hdHeight[index]}px`;
+			hiddenDiv[index].style.top = `0px`;
+			toggleIcon[index].style.transform = 'rotateZ(-180deg)'
+		}else {
+			outerHiddenDiv[index].style.height = '0px';
+			hiddenDiv[index].style.top = `-${hdHeight[index]}px`;
+			dropdownDiv[index + 1].style.marginTop = '0rem';
+			toggleIcon[index].style.transform = 'rotateZ(-360deg)';
 		}
 	}
 });
+
+// let hiddenLinksFunction = index =>{
+// 	switch (true) {
+// 		case outerHiddenDiv[index].style.height === `${hdHeight[index]}px`:
+// 		break;
+
+// 		case outerHiddenDiv[index].style.height === `0px`:
+// 		break;
+// 	}
+// }
